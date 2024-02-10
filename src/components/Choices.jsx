de8b0a6d72918611f99bfd4ddfcbc37b73db3590
Velertra <75776124+Velertra@users.noreply.github.com>
2024-02-10 02:13:00 -0700

    added summary and rating to choices.jsx. with some lite styling

--- src/components/Choices.jsx
+++ src/components/Choices.jsx
@@ -1,16 +1,29 @@
-import { useEffect, useState } from "react";
-
 const Choices = ( { show, searchedWord } ) => {
+
+    const shortSummery = (text) => {
+        if (text.length > 100) {
+          return text.substring(0, 200) + "...";
+        } else {
+          return text;
+        }
+    };
     
     return ( 
         <>
             {show ? <div>{searchedWord.map((selection, index) => {
                 return (
-                    <div key={index}>
-                        <div>{selection.title}{console.log(selection)}</div>
+                    <div style={{display: 'flex'}} key={index}>
+                        <img style={{width: '15vh'}} src={"http://image.tmdb.org/t/p/w500" + selection.poster_path}></img>
+                        <ul key={index}>
+                            <li>
+                                <h3 style={{fontSize: '2vh'}}>{selection.title || selection.name}{console.log(selection)}</h3>
+                                <p style={{fontSize: '1.4'}}>{selection.vote_average}</p>
+                                <p style={{fontSize: '1.5vh'}}>{shortSummery(selection.overview)}</p>
+                            </li>
+                        </ul>
                     </div>
                 )
-            })}</div> : <>no</>}
+            })}</div> : null}
         </>
      );
 
