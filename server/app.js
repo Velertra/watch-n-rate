fb98dc1c25d53d5c6254dedb72ea14f65f0e5d1e
Velertra <75776124+Velertra@users.noreply.github.com>
2024-07-16 21:32:04 -0700

    switched to cookie session

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const session = require("express-session");
+const session = require("cookie-session");
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
 const LocalStrategy = require("passport-local").Strategy;
